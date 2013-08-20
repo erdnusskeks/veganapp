e8805f9001a514949dc1d5c09287b7da0617818c
erdnusskeks <erdnusskeks@googlemail.com>
2013-08-20 19:36:04 +0200

    Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 VeganApp::Application.routes.draw do
   resources :places
 
-  root :to => redirect('/places')
+  root :to => 'places#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
