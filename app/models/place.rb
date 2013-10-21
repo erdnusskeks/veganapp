b0dbf50a3e55f3003cbd9174c93b9a75a50f70a9
Marco Schaden <ms@donschado.de>
2013-10-21 22:31:26 +0200

    first specs

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,7 @@
 class Place < ActiveRecord::Base
   attr_accessible :address, :city, :country, :language, :lat, :lon, :name
+
+  # write your model validations here
+  # no idea how? => http://guides.rubyonrails.org/active_record_validations.html
+
 end
