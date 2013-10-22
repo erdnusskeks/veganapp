cb4c8b7e28cdbfd2adbe6d24b17d1acb4f451be7
erdnusskeks <erdnusskeks@googlemail.com>
2013-10-22 18:00:41 +0200

    Added validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,4 +4,12 @@ class Place < ActiveRecord::Base
   # write your model validations here
   # no idea how? => http://guides.rubyonrails.org/active_record_validations.html
 
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
+  validates :language, presence: true
+  validates :name, presence: true
+  validates :lat, presence: true, numericality: true
+  validates :lon, presence: true, numericality: true
+
 end
