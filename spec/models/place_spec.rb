af08dd156ba1339fb88ec98a685fa9ad11f74e5e
Marco Schaden <ms@donschado.de>
2013-10-25 07:41:32 +0200

    place migration

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -5,10 +5,20 @@ describe Place do
   it { should validate_presence_of(:address) }
   it { should validate_presence_of(:city) }
   it { should validate_presence_of(:country) }
-  it { should validate_presence_of(:language) }
+  it { should validate_presence_of(:slug) }
   it { should validate_presence_of(:lat) }
   it { should validate_presence_of(:lon) }
 
   it { should validate_numericality_of(:lat) }
   it { should validate_numericality_of(:lon) }
+  it { should validate_numericality_of(:rating) }
+
+  describe 'attributes' do
+    it { expect(Place.column_names).to_not include('language') }
+    it { expect(Place.column_names).to include('rating') }
+    it { expect(Place.column_names).to include('slug') }
+    it { should allow_mass_assignment_of(:rating) }
+    it { should allow_mass_assignment_of(:slug) }
+  end
+
 end
